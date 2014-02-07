1c9c49213a9b81c4fb5bfc7523ef7b85eead7ba1
fstdp <fstdp@hotmail.com>
2014-02-07 20:00:08 +0100

    Create L9110.h

---
+++ L9110.h
@@ -0,0 +1,12 @@
+#ifndef L9110_h
+#define L9110_h
+
+class L9110 {
+  public:
+    L9110();
+    void forward(int l, int r, int t);
+    void reverse(int l, int r, int t);
+    void rotate(int t);
+    void brake(int t);
+};
+#endif
